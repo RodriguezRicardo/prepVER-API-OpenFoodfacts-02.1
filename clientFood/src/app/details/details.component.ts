21728569e373ccbec2cb0968be67109e51eb1c4c
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2020-12-20 19:11:01 +0000

    punto 5-6-7

--- clientFood/src/app/details/details.component.ts
+++ clientFood/src/app/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { FoodService } from '../food.service';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
+  query : string;
+  obsFood : Observable<Object>;
+  prodName : any;
+  foodServiceObs : Observable<Object>;
 
-  constructor() { }
+  constructor(public food : FoodService,
+    private route: ActivatedRoute,
+    private service: FoodService) { }
 
   ngOnInit(): void {
+    this.obsFood = this.route.paramMap;
+    this.obsFood.subscribe(this.getRouterParam);
   }
 
+  getRouterParam = (params : ParamMap) =>
+  {
+    let foodId = params.get('id');
+    console.log(foodId);
+
+    this.foodServiceObs = this.service.getDetailsID(foodId);
+    this.foodServiceObs.subscribe((data)=>this.prodName = data);
+  }
 }
--- clientFood/src/app/food.service.ts
+++ clientFood/src/app/food.service.ts
@@ -9,7 +9,16 @@ export class FoodService {
   constructor(private http: HttpClient) { }
 
   searchFood(query: string) {
-    const url = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${query}&page_size=2&json=true`;
+    const url = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${query}&page_size=5&json=true`;
+    const headers = new HttpHeaders("Content-Type");
+
+    let obsFood = this.http.get(url, { headers });
+    return obsFood;
+  }
+
+  //metodo per ottenere le informazioni del prodotto con ID
+  getDetailsID(id: string) {
+    const url = `https://world.openfoodfacts.org/api/v0/product/${id}`;
     const headers = new HttpHeaders("Content-Type");
 
     let obsFood = this.http.get(url, { headers });
