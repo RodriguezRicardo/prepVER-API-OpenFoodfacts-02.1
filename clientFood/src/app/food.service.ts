f7c49fc8d28fee4b37e48cde6a2dafb09fa7c2f3
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2020-12-20 17:49:44 +0000

    punto 4

--- clientFood/src/app/food.service.ts
+++ clientFood/src/app/food.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoodService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  searchFood(query: string) {
+    const url = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${query}&page_size=2&json=true`;
+    const headers = new HttpHeaders("Content-Type");
+
+    let obsFood = this.http.get(url, { headers });
+    return obsFood;
+  }
 }
--- clientFood/src/app/search/search.component.ts
+++ clientFood/src/app/search/search.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../food.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent  {
 
-  constructor() { }
+  query : string;
+  obsFood : Observable<Object>;
+  results : any;
 
-  ngOnInit(): void {
+  constructor(public food : FoodService) { }
+
+  submit(query : HTMLInputElement) : void {
+    if (!query.value) {
+      return;
+    }
+    this.query = query.value;
+    this.obsFood = this.food.searchFood(this.query);
+    this.obsFood.subscribe((data) => { this.results = data; console.log(this.results) });
   }
-
 }
